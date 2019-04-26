9d2ac5782ae99785e0a8456c7502927fea9acef2
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-04-26 13:48:42 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //проверить все файлы .js в проекте
+        exclude: /node_modules/, // исключение, конвертация файлов в этой папке не требуется
+        use: ["babel-loader"] //для конвертации JS-файла, написанного на ES6, в совместимый с браузером ES5
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: "./public/index.html" //путь до index.html
     })
   ],
   devServer: {
