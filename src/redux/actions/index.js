66f02d6909c74d35ae556a50d5db42eb2ce4d01f
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-06-04 21:35:39 +0400

    Create index.js

---
+++ src/redux/actions/index.js
@@ -0,0 +1,7 @@
+import { getRoster } from "./rosterActions";  
+//импортируем наше действие, а также другие ниже
+
+export { getRoster }; 
+//перечесляем действия и теперь, 
+//когда нам нужно будет вызвать действие внутри компонента,
+// делаем это так import { getRoster } from "../../redux/actions";
