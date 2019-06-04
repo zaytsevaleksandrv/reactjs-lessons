eaa002b2691611f7db3cd1c6bfd3ab2cdd977b56
Aleksandr Zaytsev <zaytsev.aleksandr.v@gmail.com>
2019-06-04 22:33:27 +0400

    Update configureStore.js

--- src/redux/stores/configureStore.js
+++ src/redux/stores/configureStore.js
@@ -1,11 +1,19 @@
+import { createBrowserHistory } from "history";
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-import rootReducer from "../reducers/index";
+import { routerMiddleware } from "connected-react-router";
+import rootReducer from "../reducers";
 
 const logger = createLogger();
+export const history = createBrowserHistory();
 
-const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  thunk,
+  routerMiddleware(history),
+  logger
+)(createStore);
 
 export default function configureStore(initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+  return createStoreWithMiddleware(rootReducer(history), initialState);
+}
